6eb6c0944c86e43db1f607d6a9be5a0db8fa750d
Aditya-2001 <iit2019210@iiita.ac.in>
2020-07-09 15:40:20 +0530

    09-07-2020__Q2 added

---
+++ 09-07-2020__Q2.py
@@ -0,0 +1,5 @@
+listt=input("Enter list: ").split(" ")
+final_list_square=list(map(lambda x: int(x)**2,listt))
+print("Square are: ",final_list_square)
+final_list_cube=list(map(lambda x: int(x)**3,listt))
+print("Cube are: ",final_list_cube)
